e35a6d302af26391aec3ca5dad17fafef4599a0d
ColorStream <amandatrinhpham@gmail.com>
2024-04-03 22:36:24 -0400

    prelim navbar and static folders

--- fashion/admin.py
+++ fashion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+"""
+app = apps.get_app_config('admin')
+
+# Auto register all models using for loop
+for model_name, model in app.models.items():
+    admin.site.register(model)
+"""
