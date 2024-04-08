f97ee34c2d2e8410cb7ca19e05d4614fc5e2440a
ColorStream <amandatrinhpham@gmail.com>
2024-04-08 02:38:54 -0400

    currently displays temperature. IT'S NOT PRETTY.

--- fashion/views.py
+++ fashion/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 import json
+import openmeteo_requests
+import requests_cache
+import pandas as pd
+from retry_requests import retry
 
 # Create your views here.
 
@@ -16,4 +20,68 @@ def weather(request):
         _type_: _description_
     """
 
-    return render(request, 'weather')
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": 27.3364,
+        "longitude": -82.5306,
+        "current": ["temperature_2m", "apparent_temperature"],
+        "daily": ["temperature_2m_max", "temperature_2m_min", "uv_index_max", "precipitation_probability_max"],
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "precipitation_unit": "inch",
+        "timezone": "America/New_York",
+        "forecast_days": 1
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+    print(f"Elevation {response.Elevation()} m asl")
+    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+    # Current values. The order of variables needs to be the same as requested.
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value()
+    current_apparent_temperature = current.Variables(1).Value()
+
+    print(f"Current time {current.Time()}")
+    print(f"Current temperature_2m {current_temperature_2m}")
+    print(f"Current apparent_temperature {current_apparent_temperature}")
+
+    # Process daily data. The order of variables needs to be the same as requested.
+    daily = response.Daily()
+    daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+    daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+    daily_uv_index_max = daily.Variables(2).ValuesAsNumpy()
+    daily_precipitation_probability_max = daily.Variables(3).ValuesAsNumpy()
+
+    daily_data = {"date": pd.date_range(
+        start = pd.to_datetime(daily.Time(), unit = "s", utc = True),
+        end = pd.to_datetime(daily.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = daily.Interval()),
+        inclusive = "left"
+    )}
+    daily_data["temperature_2m_max"] = daily_temperature_2m_max
+    daily_data["temperature_2m_min"] = daily_temperature_2m_min
+    daily_data["uv_index_max"] = daily_uv_index_max
+    daily_data["precipitation_probability_max"] = daily_precipitation_probability_max
+
+    daily_dataframe = pd.DataFrame(data = daily_data)
+    print(daily_dataframe)
+
+    context = {
+        'current_temp' : current_temperature_2m, # weather for the last 15 minutes
+        'current_apparent' : current_apparent_temperature, # what it feels like for the past 15min
+        'daily_data': daily_data,  # weather for the day
+    }
+
+    return render(request, 'fashion/weather.html', context)
