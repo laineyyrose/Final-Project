a9b26a610821d977a86b220c3e6c75532be9c9b0
ColorStream <amandatrinhpham@gmail.com>
2024-04-22 23:59:09 -0400

    minor changes, url fixes

--- fashion/urls.py
+++ fashion/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from users import views
+from users import views as userviews
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', userviews.home, name='home'),
+    path('weather/', views.weather, name='weather'),
 ]
--- shopping/urls.py
+++ shopping/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from users import views
+from users import views as userviews #this is to avoid conflicts, it fucks w the other views if not
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', userviews.home, name='home'),
+    path('listings/', views.listings, name='listings'),
 ]
