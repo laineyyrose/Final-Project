903c1b067cec2e91d79dcf98f7eec82220e7d766
ColorStream <amandatrinhpham@gmail.com>
2024-05-07 23:04:36 -0400

    pythonanywhere url edits v2

--- final_project/urls.py
+++ final_project/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='users:logout_and_redirect', permanent=False)),
+    path('', RedirectView.as_view(pattern_name='users:logout_and_redirect', permanent=True)),
     #path('', RedirectView.as_view(url='users/login/', permanent=True)),
     path("admin/", admin.site.urls),
     path('', include('shopping.urls')),
