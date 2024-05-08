00c97322922efcfcbee5e8e0b97a164d8564edb5
ColorStream <amandatrinhpham@gmail.com>
2024-05-08 14:06:28 -0400

    fixed bug on 404 w/ no profile

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib.auth import logout
@@ -9,6 +10,7 @@ from .forms import SignUpForm, EditProfileForm, ProfilePageForm
 from django.views.generic import DetailView, CreateView
 from shopping.models import Profile
 from django.shortcuts import get_object_or_404
+from django.http import Http404, HttpResponseNotAllowed
 from shopping.models import Item
 
 
@@ -29,16 +31,75 @@ class UserEditView(generic.UpdateView):
     def get_object(self):
         return self.request.user
     
-class ShowProfilePageView(DetailView):
+class ShowProfilePageView(DetailView): #LAINEY MADE THIS ONE
     model = Profile
     template_name = 'registration/user_profile.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        """Author: Andy
+        Dispatch method for the ShowProfilePageView class. This method is called when the view is loaded.
+        It overrides the dispatch method in the DetailView class to handle the case where the profile doesn't exist.
+
+        Returns:
+            self (ShowProfilePageView): The ShowProfilePageView object.
+            request (HttpRequest): The request object for the current view that DetailView bases its render off of.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments. In this case, it'll be the viewed user's primary key.
+
+        Raises:
+            Http404: Raises an exception if the profile doesn't exist to trigger the handle_profile_not_found method.
+        
+        Returns:
+            super().dispatch: Returns the dispatch method from the parent class if the profile exists.
+        """
+        if request.method == 'GET':
+            try:
+                self.object = self.get_object()
+            except Http404:
+                # Profile doesn't exist, call a separate method for handling this case
+                return self.handle_profile_not_found(request, *args, **kwargs)
+            else:
+                return super().dispatch(request, *args, **kwargs)
+        else:
+            # Handle other HTTP methods here if needed
+            return HttpResponseNotAllowed(['GET'])
+
+    def handle_profile_not_found(self, request, *args, **kwargs): #this is a custom method to handle the case where the profile doesn't exist so it doesn't 404
+        """Author: Andy
+        Handle the case where the profile doesn't exist. 
+        This method will be called after a GET when the profile doesn't exist.
+
+        Args:
+            self (ShowProfilePageView): The ShowProfilePageView object.
+            request (HttpRequest): The request object for the current view that DetailView bases its render off of.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments. In this case, it'll be the viewed user's primary key.
+
+        Returns:
+            redirect: Redirects to the show profile page, and passes the pk back through.
+        """
+        # Get the user object
+        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        # Create a new profile associated with the user
+        profile = Profile.objects.create(user=user)
+        # Redirect to the profile page
+        return redirect('show_profile_page', pk=profile.pk)
+
     def get_context_data(self, *args, **kwargs):
-        #users = Profile.objects.all()
-        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
-        user = get_object_or_404(Profile, id=self.kwargs['pk'])
-        user_items = Item.objects.all().filter(user=user.pk).order_by('-date_posted') #shows user's specific items
-        context["pageuser"] = user
+        """Author: Lainey (edited by Andy)
+        Get the context data for the ShowProfilePageView class. This method is called when the view is loaded.
+
+         Args:
+            self (ShowProfilePageView): The ShowProfilePageView object. This just passes the context to it.
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments. In this case, it'll be the viewed user's primary key.
+
+        Returns:
+            context: The context data for the ShowProfilePageView class.
+        """
+        context = super().get_context_data(*args, **kwargs)
+        user_items = Item.objects.filter(user=self.object.pk).order_by('-date_posted')
+        context["pageuser"] = self.object
         context['items'] = user_items
         return context
     
